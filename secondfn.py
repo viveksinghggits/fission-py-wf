0bf45604ad1d9be279401a87e2b3502490f77571
Vivek Singh <vivek@infracloud.io>
2018-12-18 15:13:00 +0200

    after some changes

--- secondfn.py
+++ secondfn.py
@@ -1,7 +1,9 @@
 from flask import request 
+import json
 
 def main():
 	b = request.get_data()
+	#b = json.dumps(request.get_json())
 	return "second fun \n"+ str(b)+" headers:"+str(request.headers.get("username"))+"\n"
 
 if __name__ == "__main__":
